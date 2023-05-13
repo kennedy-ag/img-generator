8f250ba99817afddc38f3f8f90c94941c6c53bfb
Kennedy Viana Aguiar <kennedycontato46@gmail.com>
2023-05-12 23:06:06 -0300

    v0.1.2 - Código reduzido e otimizado

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-from random import randint
 from matplotlib import pyplot as plt
 import numpy as np
 
-def generate_channel_list(y_size:int, x_size:int) -> list:
-    values_list = []
-    resolution = y_size * x_size
-    for i in range(resolution):
-        values_list.append(randint(0, 255))
-    return values_list
-
-X = 22
+X = 25
 Y = 20
-CHANNELS = 3
 
-reds = generate_channel_list(Y, X)
-greens = generate_channel_list(Y, X)
-blues = generate_channel_list(Y, X)
+def generate_channel_list(x:int, y:int) -> list:
+    return np.random.randint(low=0, high=255, size=y*x)
 
-lista_full = []
-lista_full.extend(reds)
-lista_full.extend(greens)
-lista_full.extend(blues)
+rgb = [generate_channel_list(X, Y) for i in range(3)]
+img = np.concatenate((rgb[0], rgb[1], rgb[2])).reshape(Y, X, 3)
 
-img = np.array(lista_full).reshape(Y, X, CHANNELS)
 plt.imshow(img)
