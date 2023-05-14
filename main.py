db66cb05a4e22ea93fd35796a3c631c0dabf5deb
Kennedy Viana Aguiar <kennedycontato46@gmail.com>
2023-05-13 22:30:45 -0300

    v0.1.3 - Adicionadas funções de filtragem básicas

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-X = 25
-Y = 20
+X = 40
+Y = 30
 
 def generate_channel_list(x:int, y:int) -> list:
     return np.random.randint(low=0, high=255, size=y*x)
 
+def reset_channel(img: np.ndarray, channel: int):
+    for line in img: 
+        for pixel in line: pixel[channel] = 0
+            
+def fill_channel(img: np.ndarray, channel: int, value=100):
+    for line in img: 
+        for pixel in line: pixel[channel] = value
+
 rgb = [generate_channel_list(X, Y) for i in range(3)]
 img = np.concatenate((rgb[0], rgb[1], rgb[2])).reshape(Y, X, 3)
+
+reset_channel(img, 2)
+fill_channel(img, 1, 40)
 
 plt.imshow(img)
